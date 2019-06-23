42357cb126093652ee131b01297a541a5cb5891c
shawn2000100 <shawn2000100@gmail.com>
2019-06-23 15:49:25 +0800

    Add files via upload

---
+++ 933. Number of Recent Calls.py
@@ -0,0 +1,18 @@
+from collections import deque
+class RecentCounter:
+    def __init__(self):
+        self.calls = deque()
+
+    def ping(self, t: int) -> int:
+        self.calls.append(t)
+        while self.calls and self.calls[0] < t - 3000:
+            self.calls.popleft()
+        return len(self.calls)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(1)
+# param_1 = obj.ping(100)
+# param_1 = obj.ping(3001)
+# param_1 = obj.ping(3002)
